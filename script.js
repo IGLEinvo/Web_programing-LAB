336cd1256fc53c59f8a20c3a560b43c7c0589813
IGLEinvo <igorlevocko@gmail.com>
2023-09-22 15:03:53 +0300

    Перший коміт

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.header__nav');
+
+burgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
