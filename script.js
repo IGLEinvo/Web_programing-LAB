4f9c2007f57039a4f0cb8e5c582223ec86a407c0
IGLEinvo <igorlevocko@gmail.com>
2023-09-28 12:48:36 +0300

    modify burger-menu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navContainer = document.querySelector('.header__nav-container');
-  
-    burgerMenu.addEventListener('click', function () {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navContainer = document.querySelector('.header__nav-container');
+
+  burgerMenu.addEventListener('click', function () {
       navContainer.classList.toggle('active');
-    });
+      burgerMenu.classList.toggle('active');
   });
-  
+});
